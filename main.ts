93c3e4a06cc96a9a0af5b501a2afd628b166b702
+和- <75563352+xiaojieyueyue@users.noreply.github.com>
2023-02-16 17:34:06 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    0,
+    1023
+    ))
+})
